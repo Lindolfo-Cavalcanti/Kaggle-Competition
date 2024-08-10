af9425f7d333575f205576672127ddf1b85195d5
Lindolfo Cavalcanti <lindolfoj47.lj@gmail.com>
2024-08-10 19:46:11 -0300

    Cleaning Done

--- Titanic/Code/Titanic-2.R
+++ Titanic/Code/Titanic-2.R
@@ -13,7 +13,30 @@ test.original$Survived = NA
 train.original$IsTrain = TRUE
 test.original$IsTrain = FALSE
 
+Age_outliers_limit = boxplot.stats(train.original$Age)$stats[5]
+
+train.original <- train.original %>%
+  filter(train.original$Age < Age_outliers_limit)
+
+Fare_outliers_limit = boxplot.stats(train.original$Fare)$stats[5]
+
+train.original <- train.original %>%
+  filter(train.original$Fare < Fare_outliers_limit)
+
 full.data = rbind(train.original, test.original)
+
+SmartEDA::ExpReport(full.data, op_file = "reportEDA.html")
+
+DataExplorer::create_report(full.data)
+dlookr::eda_web_report(full.data)
+dlookr::diagnose_web_report(full.data)
+
+
+full.data$Cabin = NULL
+
+full.data$Embarked[is.na(full.data$Embarked)] = "S"
+
+full.data$Fare[is.na(full.data$Fare)] = median(full.data$Fare)
 
 full.data$Sex = as.factor(full.data$Sex)
 full.data$Embarked = as.factor(full.data$Embarked)
@@ -21,7 +44,34 @@ full.data$Pclass = as.factor(full.data$Pclass)
 full.data$SibSp = as.factor(full.data$SibSp)
 full.data$Parch = as.factor(full.data$Parch)
 
-SmartEDA::ExpReport(full.data, op_file = "reportEDA.html")
-DataExplorer::create_report(full.data)
-dlookr::eda_web_report(full.data)
-dlookr::diagnose_web_report(full.data)
+Fare_outliers_limit_full = boxplot.stats(full.data$Fare)$stats[5]
+
+full.data <- full.data %>%
+  filter(full.data$Fare < Fare_outliers_limit_full)
+
+full.data.nosurvived = full.data
+
+full.data.nosurvived$Survived = NULL
+
+full.data.nosurvived %>% is.na() %>% colSums()
+
+full.data.nosurvived.clean = full.data.nosurvived %>% missRanger(
+  formula = Age ~ ., num.trees = 1000
+)
+
+full.data.nosurvived.clean %>% is.na() %>% colSums()
+
+new.full.data = full.data.nosurvived.clean
+
+new.full.data$Survived = full.data$Survived
+
+new.full.data %>% is.na() %>% colSums()
+
+train.clean = new.full.data%>% filter(IsTrain == TRUE)
+
+test.clean = new.full.data %>% filter(IsTrain == FALSE)
+
+train.clean$IsTrain = NULL
+
+test.clean$IsTrain = NULL
+
